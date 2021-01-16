ffc2a60b2961246eb00cae9dd613c1441c5f433a
tundera <stackshuffle@gmail.com>
2021-01-16 10:08:11 -0700

    env vars renamed

--- src/lib/datocms/index.ts
+++ src/lib/datocms/index.ts
@@ -13,7 +13,7 @@ export function request({ query, variables, preview }: RequestOptions) {
   const endpoint = preview ? 'https://graphql.datocms.com/preview' : 'https://graphql.datocms.com/'
   const client = new GraphQLClient(endpoint, {
     headers: {
-      authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`,
+      authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
     },
   })
   return client.request(query, variables)
--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {}
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+export default (_req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.setPreviewData({})
   res.writeHead(307, { Location: '/' })
   res.end()
