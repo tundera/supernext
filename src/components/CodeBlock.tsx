69c5b2ed8fa362e33e49362ecada56727ee06449
tundera <tunderadev@gmail.com>
2021-01-20 21:34:41 -0700

    finish code block

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,13 +1,20 @@
 import type { FC } from 'react'
 
+import { useState } from 'react'
 import Highlight, { defaultProps, Language } from 'prism-react-renderer'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import synthwave84 from 'prism-react-renderer/themes/synthwave84'
+
+import CopyButton from '@components/CopyButton'
+import { copyToClipboard } from '@utils/copyToClipboard'
 
 type Props = {
   codeString: string
   language: Language
 }
-const CodeBlock: FC<Props> = ({ codeString, language, ...props }) => {
+const CodeBlock: FC<Props> = ({ codeString, language, children, ...props }) => {
+  const [isCopied, setIsCopied] = useState(false)
+
   // eslint-disable-next-line react/destructuring-assignment
   if (props['react-live']) {
     return (
@@ -19,11 +26,20 @@ const CodeBlock: FC<Props> = ({ codeString, language, ...props }) => {
     )
   }
   return (
-    <Highlight {...defaultProps} code={codeString} language={language}>
+    <Highlight {...defaultProps} code={codeString} language={language} theme={synthwave84}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={style}>
+        <pre className={className} style={{ ...style, padding: '2rem', position: 'relative' }}>
+          <CopyButton
+            onClick={() => {
+              copyToClipboard(codeString)
+              setIsCopied(true)
+              setTimeout(() => setIsCopied(false), 3000)
+            }}
+          >
+            {isCopied ? '🎉 Copied!' : 'Copy'}
+          </CopyButton>
           {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
+            <div {...getLineProps({ line, key: i })} style={style}>
               {line.map((token, key) => (
                 <span {...getTokenProps({ token, key })} />
               ))}
--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,26 +1,27 @@
-import type { FC } from 'react'
-
-import { Button } from '@chakra-ui/react'
-
-const CopyButton: FC = (props) => (
-  <Button
-    position="absolute"
-    top="0"
-    right="0"
-    border="none"
-    boxShadow="none"
-    textDecoration="none"
-    margin="8px"
-    padding="8px 12px"
-    background="#E2E8F022"
-    borderRadius="8px"
-    cursor="pointer"
-    color="#E2E8F0"
-    fontSize="14px"
-    fontFamily="sans-serif"
-    lineHeight="1"
-    {...props}
-  />
-)
+const CopyButton = (props) => {
+  return (
+    <button
+      type="button"
+      style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        border: 'none',
+        boxShadow: 'none',
+        textDecoration: 'none',
+        margin: '8px',
+        padding: '8px 12px',
+        background: '#E2E8F022',
+        borderRadius: '8px',
+        cursor: 'pointer',
+        color: '#E2E8F0',
+        fontSize: '14px',
+        fontFamily: 'sans-serif',
+        lineHeight: '1',
+      }}
+      {...props}
+    />
+  )
+}
 
 export default CopyButton
