090c8d9bb7dcfd5e2fbaf576eab3ea14f09e0a58
tundera <tunderadev@gmail.com>
2021-01-26 11:41:54 -0700

    commit

--- pages/teams/index.tsx
+++ pages/teams/index.tsx
@@ -2,7 +2,7 @@ import type { GetStaticProps } from 'next'
 
 import { getDataHooksProps } from 'next-data-hooks'
 
-import TeamsIndex from '@routes/teams'
+import TeamsIndex from '@routes/blog'
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const dataHooksProps = await getDataHooksProps({
