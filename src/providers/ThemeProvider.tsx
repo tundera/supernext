9ba6762fd92ddf6995d0d93b1c037871ff730637
tundera <tunderadev@gmail.com>
2021-01-27 09:25:16 -0700

    commit

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -26,14 +26,3 @@ const ThemeProvider: FC<Props> = ({ cookies, children }) => {
 }
 
 export default ThemeProvider
-
-// also export a reusable function getServerSideProps
-export function getServerSideProps({ req }) {
-  return {
-    props: {
-      // first time users will not have any cookies and you may not return
-      // undefined here, hence ?? is necessary
-      cookies: req.headers.cookie ?? '',
-    },
-  }
-}
