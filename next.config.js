9b1ebb8e9606932203088b5ece965ee69a01b191
tundera <tunderadev@gmail.com>
2021-02-05 18:59:23 -0700

    enable webpack 5

--- next.config.js
+++ next.config.js
@@ -2,26 +2,18 @@ module.exports = {
   experimental: {
     reactMode: 'concurrent',
   },
-  // future: {
-  //   webpack5: true,
-  // },
+  future: {
+    webpack5: true,
+  },
   target: 'experimental-serverless-trace',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
   },
   images: {
-    // loader: 'cloudinary',
-    // path: 'https://res.cloudinary.com/dbc3x3s7c/image/upload/',
     domains: ['cdn.sanity.io'],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      }
-    }
-
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
