d91591a8824f2f2ac08f4d85342d359c77b9b923
tundera <tunderadev@gmail.com>
2021-02-06 11:26:33 -0700

    add metrics rewrite

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -1,6 +1,5 @@
 import { GraphQLClient } from 'graphql-request'
 import { useQuery, UseQueryOptions } from 'react-query'
-
 export type Maybe<T> = T | null
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] }
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> }
