c7450b002a6b808065ddc71c38fc01de06136edb
tundera <tunderadev@gmail.com>
2021-02-05 19:44:23 -0700

    add metrics rewrite

--- src/components/ui/lists/TeamsList.tsx
+++ src/components/ui/lists/TeamsList.tsx
@@ -8,8 +8,8 @@ function TeamsList({ teams }) {
     <CloudinaryContext cloudName="dbc3x3s7c">
       <List spacing={3}>
         {teams.map((team) => {
-          const [{ primary, secondary }] = team.colorScheme
-          const bg = `linear-gradient(108deg, ${secondary ?? ''} 0%, ${primary ?? ''} 65%)`
+          const [colors] = team.colorScheme
+          const bg = `linear-gradient(108deg, ${colors?.secondary ?? ''} 0%, ${colors?.primary ?? ''} 65%)`
           return (
             <ListItem py="4">
               <TeamCard
