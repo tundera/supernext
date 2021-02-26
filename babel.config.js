8c241e4b1991dda18948c5878f2b42d582a5275c
tundera <tunderadev@gmail.com>
2021-02-26 09:33:28 -0700

    test

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,21 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     'babel-plugin-parameter-decorator',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@graphql': './src/graphql',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@providers': './src/providers',
+          '@lib': './src/lib',
+          '@styles': './styles',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'],
+      },
+    ],
     'superjson-next',
     'babel-plugin-macros',
   ],
